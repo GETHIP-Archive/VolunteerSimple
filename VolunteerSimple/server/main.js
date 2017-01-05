7be3b6b29cbcbc0ebca6e88eebaca28e22a46a49
Abby Brooks <Abby Brooks>
2017-01-05 12:06:07 -0600

    addition of opportunity template and save button

--- VolunteerSimple/server/main.js
+++ VolunteerSimple/server/main.js
@@ -3,15 +3,5 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 
-  /*
-  //save button mouseover- i am no expert, so idk how to make this work, an idea though
-  $(document).ready(function () {
-    $(document).on('mouseenter', '.divbutton', function () {
-        $(this).find(":button").show();
-    }).on('mouseleave', '.divbutton', function () {
-        $(this).find(":button").hide();
-    });
-  });
-  */
 
 });
