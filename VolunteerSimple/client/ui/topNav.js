02cc7ee5d28d121ca7cc277bb50a0a75ba57bbfa
Abby Brooks <Abby Brooks>
2017-02-04 11:52:32 -0600

    nav links

---
+++ VolunteerSimple/client/ui/topNav.js
@@ -0,0 +1,17 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './topNav.html';
+import '../../lib/routes.js'
+
+Template.topNav.events ({
+  'click .dashboard': function() {
+    FlowRouter.go('/');
+  },
+  'click .saved': function(){
+    FlowRouter.go('/schedule');
+  },
+  'click .messages': function(){
+    FlowRouter.go('/messages');
+  }
+})
