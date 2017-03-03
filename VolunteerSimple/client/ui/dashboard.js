571a5b30ca36f78bc87d02234962ff22ea934cba
Jcbgid <jgid261@hotmail.com>
2017-03-02 20:47:30 -0600

    Hotfix to remove excessive console.log()

--- VolunteerSimple/client/ui/dashboard.js
+++ VolunteerSimple/client/ui/dashboard.js
@@ -8,21 +8,15 @@ Meteor.subscribe("Clients");
 
 Template.dashboard.helpers({
     post: function() {
-      console.log(Opportunity.find().fetch);
-
         var result = Opportunity.find().fetch();
         var saved = Clients.findOne({account: Meteor.user()._id}).saved;
-        console.log(saved);
-        console.log(result);
         for(var i = 0; i < result.length; i++){
-          console.log(saved.includes(result[i]._id));
         if(saved.includes(result[i]._id)){
           result[i].stat = false;
         }else{
           result[i].stat = true;
           }
         }
-        console.log(result);
         return result;
     }
 });
--- VolunteerSimple/client/ui/opportunity.js
+++ VolunteerSimple/client/ui/opportunity.js
@@ -16,16 +16,11 @@ Template.opportunity.helpers({
 
 Template.opportunity.events({
   'click .save': function (event){
-    console.log(Clients.findOne({account: Meteor.user()._id}));
     if(Clients.findOne({account: Meteor.user()._id}).saved.includes(event.target.id)){
        Meteor.call("modSave", Meteor.user()._id, event.target.id, "remove");
-       console.log("add");
     }else{
       Meteor.call("modSave", Meteor.user()._id, event.target.id, "add");
-      console.log("remove");
     }
-     console.log(event.target)
-     console.log("sfd");
   },
   'click .details': function(event){
     FlowRouter.go('/details/' + event.target.id);
