bea677fab3a0ffb508215da31e7064d5ff9d8f24
Jcbgid <jgid261@hotmail.com>
2017-03-02 20:45:29 -0600

    Opportunity Saving functionality

--- VolunteerSimple/client/ui/dashboard.js
+++ VolunteerSimple/client/ui/dashboard.js
@@ -1,10 +1,28 @@
 import { Opportunity }  from '../../lib/opportunity.js'
+import { Clients }  from '../../lib/client.js'
+
 
 Meteor.subscribe("Opportunity");
+Meteor.subscribe("Clients");
+
 
 Template.dashboard.helpers({
     post: function() {
       console.log(Opportunity.find().fetch);
-        return Opportunity.find();
+
+        var result = Opportunity.find().fetch();
+        var saved = Clients.findOne({account: Meteor.user()._id}).saved;
+        console.log(saved);
+        console.log(result);
+        for(var i = 0; i < result.length; i++){
+          console.log(saved.includes(result[i]._id));
+        if(saved.includes(result[i]._id)){
+          result[i].stat = false;
+        }else{
+          result[i].stat = true;
+          }
+        }
+        console.log(result);
+        return result;
     }
 });
--- VolunteerSimple/client/ui/opportunity.js
+++ VolunteerSimple/client/ui/opportunity.js
@@ -1,14 +1,31 @@
+import { Clients }  from '../../lib/client.js'
+
 import '../../lib/routes.js'
+
+Meteor.subscribe("Clients");
+
 
 Template.opportunity.helpers({
     stream: function() {
         return Opprounity.find();
+    },
+    status: function(){
+
     }
 });
 
 Template.opportunity.events({
   'click .save': function (event){
-    // Meteor.call("") **To add to save**
+    console.log(Clients.findOne({account: Meteor.user()._id}));
+    if(Clients.findOne({account: Meteor.user()._id}).saved.includes(event.target.id)){
+       Meteor.call("modSave", Meteor.user()._id, event.target.id, "remove");
+       console.log("add");
+    }else{
+      Meteor.call("modSave", Meteor.user()._id, event.target.id, "add");
+      console.log("remove");
+    }
+     console.log(event.target)
+     console.log("sfd");
   },
   'click .details': function(event){
     FlowRouter.go('/details/' + event.target.id);
