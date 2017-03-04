c3dbaa091a441af08a560427bc404aa3d867cf92
lucass a <lucass a>
2017-03-04 08:51:00 -0600

    Added details page

--- VolunteerSimple/lib/routes.js
+++ VolunteerSimple/lib/routes.js
@@ -71,6 +71,13 @@ FlowRouter.route('/information', {
   }
 });
 
+FlowRouter.route('/manage', {
+  name: "signup",
+  action: function() {
+    BlazeLayout.render("content", {content: "manage"});
+  }
+});
+
 FlowRouter.route('/create', {
   name: "sHome",
   action: function() {
